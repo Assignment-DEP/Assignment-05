99a4a14f32d579fffbbe08d60f35aedcba922ffb
Dewuruge <dewurugekalpadeep@gmail.com>
2023-08-14 10:09:54 +0530

    Final

--- Algo_3.java
+++ Algo_3.java
@@ -4,12 +4,9 @@ public class Algo_3 {
     public static void main(String[] args) {
         int[] nums = {10, 20, 30, 40, 50};
         int[] oppositeNUms = new int[nums.length];
-        int j = nums.length-1;
-
         for (int i = 0; i < oppositeNUms.length; i++) {
-            oppositeNUms[i]=nums[j];
-            j--;
-            
+            oppositeNUms[i]=nums[nums.length-1-i];
+ 
         }
         nums = oppositeNUms;
         System.out.println(Arrays.toString(nums));
